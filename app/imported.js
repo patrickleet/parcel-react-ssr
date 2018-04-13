99a0556ab33c1b99743c6a93139cf86929d7ffdc
Patrick Scott <pat@patscott.io>
2018-04-13 04:19:42 -0400

    chore: latest react-imported-component renders slightly differently

--- app/imported.js
+++ app/imported.js
@@ -1,7 +1,7 @@
 /* eslint-disable */ 
     import {assignImportedComponents} from 'react-imported-component';
     const applicationImports = {
-      "0": () => import('./pages/HelloWorld2'),
+      0: () => import('./pages/HelloWorld2'),
     };
     assignImportedComponents(applicationImports);
     export default applicationImports;
